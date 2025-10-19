75131336220fc17acc61d65925566f3eaa10c857
Khairil Azmi Ashari <khairilazmiashari@gmail.com>
2025-10-19 23:04:30 +0700

    chore: comprehensive project cleanup 2025 - removed 157+ unnecessary files

--- frontend/src/views/plugin/Context.js
+++
@@ -1,5 +0,0 @@
-import { createContext } from 'react';
-
-export const ProfileContext = createContext();
-export const CartContext = createContext();
-export const WishlistContext = createContext();
