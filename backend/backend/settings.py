feabc51a9c2e1c5f3365801fb7bbaab10ac1d572
Khaz Dev <khaz-dev@github.com>
2026-06-15 10:24:40 +0700

    try fix BE Admin Logo on staging server

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -205,10 +205,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, "static"),
-    os.path.join(BASE_DIR.parent, "frontend", "src", "assets"),
-]
+STATICFILES_DIRS = [os.path.join(BASE_DIR, "static")]
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 MEDIA_URL = '/media/'
